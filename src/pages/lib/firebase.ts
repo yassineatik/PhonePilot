fce63c19ee58225a4184b8fc7cad7eb16733d465
Yassine Atik <brucexx02@gmail.com>
2023-04-21 01:24:01 +0000

    Designer login and register pages

--- src/pages/lib/firebase.ts
+++ src/pages/lib/firebase.ts
@@ -19,9 +19,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-export { app, auth, db };
